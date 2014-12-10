52547446593c5099ba984cd42debc87cb7c1c4a2
neyric <eric.abouaf@gmail.com>
2014-12-10 19:00:13 +0100

    Store activities in lib/

---
+++ README.md
@@ -0,0 +1,13 @@
+# MySQL query activity for Mogy
+
+Check out the [mogy project](https://github.com/neyric/mogy)
+
+## Installation
+
+In your mogy project, install the dependency using npm :
+
+    $ npm install mogy-mysql --save
+
+The launch the registration to Amazon Simple Workflow :
+
+    $ mogy register
--- index.js
+++ index.js
@@ -1,28 +1,2 @@
-var mysql = require('mysql');
 
-
-var mysql_query = function (input, config, cb) {
-
-    var connection = mysql.createConnection(config);
-    connection.connect(function(err) {
-
-        if (err) { return cb(err); }
-
-        connection.query(input.query, function(err, rows, fields) {
-
-            connection.end();
-
-            if (err) {
-                cb(err);
-                return;
-            }
-
-            cb(null, rows);
-
-        });
-
-    });
-
-};
-
-exports.mysql = mysql_query;
+exports.mysql = require('./lib/mysql');
